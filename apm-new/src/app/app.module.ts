f54944242bc8607124c646498f737970b6f52613
Daniel Halverson <daniel.halverson@slalom.com>
2022-06-16 10:25:40 -0600

    Adds form module for filter by search

--- apm-new/src/app/app.module.ts
+++ apm-new/src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule, ɵisListLikeIterable } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

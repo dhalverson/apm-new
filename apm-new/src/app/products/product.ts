702cf4765961fbf92314f4028a09a45341027832
Daniel Halverson <daniel.halverson@slalom.com>
2022-06-16 13:16:32 -0600

    Implements product interface

---
+++ apm-new/src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productName: string;
+  productId: number;
+  productCode: string;
+  releaseDate: string;
+  description: string;
+  price: number;
+  starRating: number;
+  imageUrl: string;
+}

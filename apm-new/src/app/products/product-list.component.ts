bf3b02ad0d733eb2fc7a587a76de81f4207a42fd
Daniel Halverson <daniel.halverson@slalom.com>
2022-06-16 15:08:02 -0600

    Adds onInit lifecycle hook as placeholder for later

--- apm-new/src/app/products/product-list.component.ts
+++ apm-new/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from "./product";
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -69,4 +69,7 @@ export class ProductListComponent {
     this.showImage = !this.showImage;
   }
 
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
 }

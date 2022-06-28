2e51dd77a8e8d99c84430ee8aa18cc953fabc8f4
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2022-06-28 12:37:49 -0600

    Adds products.json file to mock database

--- apm-new/src/app/products/product.service.ts
+++ apm-new/src/app/products/product.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Product } from './product';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductService {
+
+  constructor(private http: HttpClient) {}
 
   getProducts(): Product[] {
     return [

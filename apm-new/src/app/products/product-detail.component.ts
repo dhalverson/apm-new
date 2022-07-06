e6b1796ab42dd29791a6339d4e2192ebd3542ccf
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2022-07-06 13:52:06 -0600

    Adds star rating child component to product details

--- apm-new/src/app/products/product-detail.component.ts
+++ apm-new/src/app/products/product-detail.component.ts
@@ -8,7 +8,16 @@ import { Product } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  product: Product | undefined ;
+  product: Product | undefined =   {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2021",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+  } ;
 
   constructor(private route: ActivatedRoute) { }
 
